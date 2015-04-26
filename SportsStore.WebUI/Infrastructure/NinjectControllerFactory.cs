df16167c1b5f302e71f95829b71c930121cfe6aa
prozoroff <alexprozoroff@gmail.com>
2015-04-27 00:17:36 +0300

    started

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Moq;
+using System.Collections.Generic;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    // реализация пользовательской фабрики контроллеров,
+    // наследуясь от фабрики используемой по умолчанию
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            // создание контейнера
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,
+        Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            // конфигурирование контейнера
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 }
+            }.AsQueryable());
+                        
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
